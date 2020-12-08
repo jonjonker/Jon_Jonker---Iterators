cb110335417818af99c51164d515f970a7dc6d16
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2020-12-08 12:00:20 +0100

    Add files via upload

---
+++ iterators.js
@@ -0,0 +1,47 @@
+// let colors = ["yellow", "blue", "red", "orange"];
+
+
+// While Loop
+
+// let index = 0;
+// while (index < colors.length) {
+//     console.log(colors[index]);
+//     index ++;
+// }
+
+// console.log('\n');
+
+// For Loop
+// for (let index = 0; index < colors.length; index++) {
+//     const element = colors[index];
+//     console.log(element);
+// }
+
+// console.log('\n');
+
+//forEach loop
+
+// const array1 = ['yellow', 'blue', 'red', 'orange'];
+
+// array1.forEach(element => console.log(element));
+
+//1. 7 regels
+//2. 2 regels
+//3. array loop is makkelijker leesbaar. Alle info staat in de forEach code ipv over verschillende regels. 
+//4. 
+
+// let person = {
+//     name: "Jon", 
+//     age: 31,
+//     Birthday: "09-05-1989",
+//     City: "Breda",
+//     Country: "Netherlands"
+// }
+
+// let entries = Object.entries(person)
+// console.log(person)
+
+// const keys = Object.keys(person)
+// for (const key of keys) {
+//   console.log(key)
+// }
